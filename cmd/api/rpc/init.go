d6930df48acd2c9a18d3fb46b0ee90034adf7261
Gwen0x4c3 <932713272@qq.com>
2024-12-10 14:03:27 +0800

    refactor(rpc): change the way of handling rpc errors

--- cmd/api/rpc/init.go
+++ cmd/api/rpc/init.go
@@ -1,20 +1,6 @@
 package rpc
 
-import (
-    "github.com/gwen0x4c3/easy_note/pkg/errno"
-)
-
 func InitRPC() {
     InitUserRPC()
     InitNoteRPC()
-}
-
-func checkError(code int64, message string, err error) error {
-    if err != nil {
-        return err
-    }
-    if code != errno.SuccessCode {
-        return errno.NewErrNo(code, message)
-    }
-    return nil
 }
--- cmd/note/rpc/init.go
+++ cmd/note/rpc/init.go
@@ -1,17 +1,5 @@
 package rpc
 
-import "github.com/gwen0x4c3/easy_note/pkg/errno"
-
 func InitRPC() {
     InitUserRPC()
-}
-
-func checkError(code int64, message string, err error) error {
-    if err != nil {
-        return err
-    }
-    if code != errno.SuccessCode {
-        return errno.NewErrNo(code, message)
-    }
-    return nil
 }
